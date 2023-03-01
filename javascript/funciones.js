af46641d124ede2d0b06501c8926ca05ac06cd8b
franklin asus <franksaccomandi@gmail.com>
2023-03-01 03:53:32 -0300

    entrega proyecto final

---
+++ javascript/funciones.js
@@ -0,0 +1,42 @@
+/*
+document.getElementById('descripcionteodoro').addEventListener('click' , function() {
+    console.log ("hola mundo");
+    document.getElementById("descripcion").innerHTML = "por favor funciona"
+}); 
+*/
+
+document.getElementById('iconouser').addEventListener('click' , function() {
+    console.log ("cambio de informacion");
+    document.getElementById("titulo2").innerHTML = "Hola, mi nombre es"
+    document.getElementById("titulo3").innerHTML = "TEODORO HAMILTON"
+}); 
+
+document.getElementById('iconoemail').addEventListener('click' , function() {
+    console.log ("cambio de informacion");
+    document.getElementById("titulo2").innerHTML = "Mi email es"
+    document.getElementById("titulo3").innerHTML = "emailexample@example.com"
+});
+
+document.getElementById('iconocalendario').addEventListener('click' , function() {
+    console.log ("cambio de informacion");
+    document.getElementById("titulo2").innerHTML = "Mi cumpleaños es"
+    document.getElementById("titulo3").innerHTML = "23 / 02 / 1990"
+});
+
+document.getElementById('iconomapa').addEventListener('click' , function() {
+    console.log ("cambio de informacion");
+    document.getElementById("titulo2").innerHTML = "Mi direccion es"
+    document.getElementById("titulo3").innerHTML = "1775 Paddock Way"
+});
+
+document.getElementById('iconotelefono').addEventListener('click' , function() {
+    console.log ("cambio de informacion");
+    document.getElementById("titulo2").innerHTML = "Mi número telefónico es"
+    document.getElementById("titulo3").innerHTML = "(234) 252-8011"
+});
+
+document.getElementById('iconocontrasena').addEventListener('click' , function() {
+    console.log ("cambio de informacion");
+    document.getElementById("titulo2").innerHTML = "Mi contraseña es"
+    document.getElementById("titulo3").innerHTML = "helmet"
+});
